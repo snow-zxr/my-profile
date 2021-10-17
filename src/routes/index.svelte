0a8645a7af917c901776d6648aee585c74fa5e00
snow-zxr <c.snow0908@gmail.com>
2021-10-17 10:46:56 +0000

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Color from '$lib/Color.svelte';
+    let colorName1 = 'pink';
+    let colorName2 = 'lightblue';
+</script>
+
+<div>
+    <h1 class="text-primary">Play with Colors</h1>
+</div>
+
+<div>
+    <input bind:value={colorName1} />
+    <Color color={colorName1} />
+</div>
+
+<div>
+    <input bind:value={colorName2} />
+    <Color color={colorName2} />
+</div>
+
+<style>
+    input {
+   	 padding: 0.5em;
+   	 text-align: center;
+    }
+    div {
+   	 text-align: center;
+   	 margin: 3em auto;
+    }
+</style>
