c87be3faf39d4fcc7fef2b43342f08bfae12653e
snow-zxr <c.snow0908@gmail.com>
2021-10-17 10:56:53 +0000

    first commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,47 @@
 <script>
-    import Color from '$lib/Color.svelte';
-    let colorName1 = 'pink';
-    let colorName2 = 'lightblue';
+    import Color from '$lib/style.svelte';
+    let Profile = "lightblue"
 </script>
 
 <div>
-    <h1 class="text-primary">Play with Colors</h1>
+    <h1 class="text-primary">My Profile</h1>
+    <h1>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</h1>
 </div>
 
-<div>
-    <input bind:value={colorName1} />
-    <Color color={colorName1} />
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item ">
+          <a class="nav-link text-light" href="#about">About <span class="sr-only">(current)</span></a>
+        </li>
+    </ul>
+</div>    
+
+</nav>
+<div class="jumbotron  text-center">
+  <img src="images/collagemaker_202028194033865.jpg" class="rounded-circle img-fluid" alt="">
+  <h2 class="text-center">Snow Chow</h1>
+  <hr class="my-2">
+  <p class="text-center">I love information technology and would like to learn more about it if I have opportunity</p>
 </div>
 
-<div>
-    <input bind:value={colorName2} />
-    <Color color={colorName2} />
+</section>
+<section id="about">
+<div class="container">
+  <div class="row">
+  <div class="col-md-6">
+    <img src="images/IMG-20200831-WA0018.jpg" class="rounded-0" alt="">
+  </div>
+  <div class="col-md-6">
+    <h3>Discover</h3>
+    <h4>About Me</h4>
+    <h6>________________________________________________________________________________________________________________</h6>
+    <br>
+    <br>
+    <p>My name is Snow Chow.</p>
+    <p>I am 13 years old.</p>
+    <p>I study at CHUNG LING PRIVATE HIGH SCHOOL.</p>
+    <p>I like to swimming and listen music.</p>
+  </div>
+  </div>
 </div>
-
-<style>
-    input {
-   	 padding: 0.5em;
-   	 text-align: center;
-    }
-    div {
-   	 text-align: center;
-   	 margin: 3em auto;
-    }
-</style>
+</section>
